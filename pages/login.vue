f9f82bbf3dac2e028d32aa8bcbacddbf656daa52
netervati <netervati@gmail.com>
2023-02-09 22:03:23 +0800

    Integrate supabase to pages

--- pages/login.vue
+++ pages/login.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
-  definePageMeta({ auth: false });
+  const user = useSupabaseUser();
+  const { auth } = useSupabaseAuthClient();
 
-  const { signIn } = useSession();
+  watchEffect(() => {
+    if (user.value) {
+      navigateTo('/');
+    }
+  });
 </script>
 
 <template>
   <section class="bg-base-300 flex h-screen">
     <article class="bg-base-100 card m-auto p-6 shadow-xl w-96">
-      <button class="btn" type="button" @click="signIn('github')">
-        Sign in using Github
+      <button
+        class="btn"
+        type="button"
+        @click="auth.signInWithOAuth({ provider: 'github' })"
+      >
+        Sign in with Github
       </button>
     </article>
   </section>
