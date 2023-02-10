2525f1a95b9447c2491cf998fd0efe49837955c9
netervati <netervati@gmail.com>
2023-02-10 09:38:31 +0800

    Remove sign out button

--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,8 @@
   definePageMeta({
     middleware: 'auth',
   });
-
-  const client = useSupabaseAuthClient();
-
-  const handleLogout = async () => {
-    await client.auth.signOut();
-
-    navigateTo('/login');
-  };
 </script>
 
 <template>
-  <section>
-    <button @click="handleLogout">Sign Out</button>
-    Protected
-  </section>
+  <section>Protected</section>
 </template>
