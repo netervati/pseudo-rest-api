7b4451f675b5e9362e7d09fb68f1d3f60dd33631
netervati <netervati@gmail.com>
2023-02-15 02:17:14 +0800

    Add user authentication validation

---
+++ server/validations/auth.validations.ts
@@ -0,0 +1,9 @@
+import type { H3Event } from 'h3';
+import get from 'lodash/get';
+import { NotAuthenticatedError } from '../errors';
+
+export function authValidation(event: H3Event): Result<never, APIError> {
+  if (get(event, 'context.auth.user', null) === null) {
+    return NotAuthenticatedError;
+  }
+}
---
+++ server/validations/index.ts
@@ -0,0 +1,1 @@
+export { authValidation } from './auth.validations';
