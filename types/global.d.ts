fa5ea96fdb5cbad56cb08a51add930cc5a91119e
netervati <netervati@gmail.com>
2023-02-15 02:18:28 +0800

    Support generic and validation result type

--- types/global.d.ts
+++ types/global.d.ts
@@ -3,6 +3,9 @@ declare global {
     statusCode: number;
     statusMessage: string;
   }
+
+  type Result<T, E> = Ok<T, E> | Err<T, E>;
+  type ValidationResult = Result<void, APIError>;
 }
 
 export {};
