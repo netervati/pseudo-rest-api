01fc8f5bda062e80a087049676111701b989a86c
netervati <netervati@gmail.com>
2023-02-16 04:15:59 +0800

    Create validation helpers

---
+++ server/validations/helpers/index.ts
@@ -0,0 +1,33 @@
+import isEmpty from 'lodash/isEmpty';
+
+type AnyValue = string | boolean | number;
+
+const VALIDATION_RULES: { [key: string]: (value: AnyValue) => boolean } = {
+  blank: (value) => {
+    if (typeof value !== 'string') {
+      return false;
+    }
+
+    return value.trim() === '';
+  },
+  required: (value) => isEmpty(value),
+  string: (value) => {
+    if (value === null || value === undefined) {
+      return false;
+    }
+
+    return typeof value !== 'string';
+  },
+};
+
+export function validateByRules(validations: string, value: AnyValue) {
+  const validationRules = validations.split(',');
+
+  validationRules.forEach((rule) => {
+    if (VALIDATION_RULES[rule](value)) {
+      return true;
+    }
+  });
+
+  return false;
+}
