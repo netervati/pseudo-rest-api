3e6c6d0ee92ca0abdf92542cbe5ec25444c4ba54
netervati <netervati@gmail.com>
2023-02-17 00:20:40 +0800

    Create input component

---
+++ components/input.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+  const emit = defineEmits<{
+    (e: 'change'): void;
+    (e: 'update:modelValue', value: string): void;
+  }>();
+
+  interface Props {
+    class?: string;
+    disabled?: boolean;
+    error?: boolean;
+    modelValue: string;
+    placeholder?: string;
+  }
+
+  const props = withDefaults(defineProps<Props>(), {
+    class: '',
+    disabled: false,
+    error: false,
+    placeholder: '',
+  });
+
+  const mergedClass = computed(() => {
+    return {
+      [props.class]: props.class !== '',
+      'input-error': props.error,
+    };
+  });
+
+  const inputValue = computed({
+    get() {
+      return props.modelValue;
+    },
+    set(value: string) {
+      emit('change');
+      emit('update:modelValue', value);
+    },
+  });
+</script>
+
+<template>
+  <input
+    v-model="inputValue"
+    class="input input-bordered w-full max-w-vs"
+    type="text"
+    :class="mergedClass"
+    :disabled="disabled"
+    :placeholder="placeholder"
+  />
+</template>
