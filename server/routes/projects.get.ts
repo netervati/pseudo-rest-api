cce7198e9f5a3730b2eff26f73d716e06ae8dd0b
netervati <netervati@gmail.com>
2023-02-19 02:31:40 +0800

    Create get projects endpoint

---
+++ server/routes/projects.get.ts
@@ -0,0 +1,54 @@
+import { H3Event } from 'h3';
+import { authValidation } from '../validations';
+import { BaseError, FailedDatabaseQueryError } from '../errors';
+import { Database } from '~~/types/supabase';
+import { serverSupabaseClient } from '#supabase/server';
+
+export default defineEventHandler(async (event) => {
+  const validateError = await validate(event);
+
+  if (validateError) {
+    throw createError({
+      statusCode: validateError.statusCode,
+      statusMessage: validateError.statusMessage,
+    });
+  }
+
+  const response = await handleRequest(event);
+
+  if (response instanceof BaseError) {
+    throw createError({
+      statusCode: response.statusCode,
+      statusMessage: response.statusMessage,
+    });
+  }
+
+  return {
+    data: response,
+  };
+});
+
+async function handleRequest(
+  event: H3Event
+): Promise<Result<string, APIError>> {
+  const client = serverSupabaseClient<Database>(event);
+
+  const { data: projects, error: projectError } = await client
+    .from('projects')
+    .select('id, description, name')
+    .eq('is_deleted', false);
+
+  if (projectError) {
+    return new FailedDatabaseQueryError('Failed to retrieve projects.');
+  }
+
+  return projects.map((project) => ({ attributes: { ...project } }));
+}
+
+function validate(event: H3Event): ValidationResult {
+  const error = authValidation(event);
+
+  if (error) {
+    return error;
+  }
+}
