a527397c1c8be82b166fc671eb0551e494b320f3
netervati <netervati@gmail.com>
2023-02-18 22:32:28 +0800

    Utilize useModal and display secret key on success

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,30 @@
 <script lang="ts" setup>
+  import ModalCreateProject from '~~/components/modal/createProject.vue';
+
   definePageMeta({
     middleware: 'auth',
+  });
+
+  const secretKey = ref('');
+
+  const modal = useModal(ModalCreateProject, {
+    onSuccess: (key: string) => {
+      secretKey.value = key;
+    },
   });
 </script>
 
 <template>
   <div class="p-6">
-    <Button color="success" size="sm">New Project</Button>
+    <Button color="success" size="sm" @click="modal.open">New Project</Button>
+    <article v-if="secretKey !== ''" class="bg-emerald-400 p-8 rounded-lg mt-4">
+      <p>
+        Below is your project's secret key. Make sure to copy this as you will
+        only see this once!
+      </p>
+      <div class="mt-2">
+        <input class="bg-emerald-300 input w-full" :value="secretKey" />
+      </div>
+    </article>
   </div>
 </template>
