57b7f26541cf25582db360c32f0b39be9253d7f7
netervati <netervati@gmail.com>
2023-02-19 13:42:38 +0800

    Utilize heroicons

--- components/side/bar.vue
+++ components/side/bar.vue
@@ -1,3 +1,28 @@
+<script lang="ts" setup>
+  import { HomeIcon, ServerStackIcon } from '@heroicons/vue/24/outline';
+
+  const links = [
+    { name: 'Home', to: '/' },
+    { name: 'APIs', to: '/apis' },
+  ];
+
+  const iconClass = 'h-4 w-4';
+</script>
+
 <template>
-  <div class="border-r border-gray-300 h-screen shadow-xl" />
+  <div
+    class="border-r border-gray-300 h-screen shadow-xl flex flex-col items-start"
+  >
+    <div
+      v-for="link in links"
+      :key="link.to"
+      class="mr-auto ml-auto mt-2 tooltip tooltip-right"
+      :data-tip="link.name"
+    >
+      <Button color="ghost" @click="navigateTo(link.to)">
+        <HomeIcon v-if="link.to === '/'" :class="iconClass" />
+        <ServerStackIcon v-if="link.to === '/apis'" :class="iconClass" />
+      </Button>
+    </div>
+  </div>
 </template>
