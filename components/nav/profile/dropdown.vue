3ec198e3c373bb7b29d0b702e4ab7123953da8d8
netervati <netervati@gmail.com>
2023-02-19 13:42:03 +0800

    Alphabetize classes

--- components/nav/profile/dropdown.vue
+++ components/nav/profile/dropdown.vue
@@ -1,7 +1,7 @@
 <template>
   <ul
     tabindex="0"
-    class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+    class="bg-base-100 dropdown-content menu menu-compact mt-3 p-2 rounded-box shadow w-52"
   >
     <slot />
   </ul>
