eb70470ae8dc268e7c2e610f2489eed171c14650
netervati <netervati@gmail.com>
2023-02-20 18:39:50 +0800

    Reorder if blocks

--- server/validations/apisValidations.ts
+++ server/validations/apisValidations.ts
@@ -4,14 +4,14 @@ import { validateByRules } from './helpers';
 type BodyParams = { [key: string]: string };
 
 export function postApiValidation({
-  urlPath,
   description,
+  urlPath,
 }: BodyParams): Result<never, APIError> {
-  if (validateByRules('required,string,blank', urlPath)) {
-    return new InvalidParameterError('urlPath');
-  }
-
   if (validateByRules('string', description)) {
     return new InvalidParameterError('description');
+  }
+
+  if (validateByRules('required,string,blank', urlPath)) {
+    return new InvalidParameterError('urlPath');
   }
 }
