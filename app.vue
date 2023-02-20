fd6081cc8572da8d29e72b843200491ae7a5640f
netervati <netervati@gmail.com>
2023-02-20 18:42:01 +0800

    Mount modal container

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
+<script lang="ts" setup>
+  onMounted(() => {
+    const element = document.createElement('div');
+    element.setAttribute('id', 'modal-container');
+    document.body.append(element);
+
+    return element;
+  });
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
