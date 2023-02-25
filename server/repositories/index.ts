d3a9c9bea78960c4ca55b24f1601490c00b9508d
netervati <netervati@gmail.com>
2023-02-25 21:31:24 +0800

    Fix export name

--- server/repositories/index.ts
+++ server/repositories/index.ts
@@ -2,4 +2,4 @@ export { default as ApiRepository } from './apiRepository';
 export { default as ProjectKeyRepository } from './projectKeyRepository';
 export { default as ProjectRepository } from './projectRepository';
 export { default as ResourceDataRepository } from './resourceDataRepository';
-export { default as ResourceModelTable } from './resourceModelRepository';
+export { default as ResourceModelRepository } from './resourceModelRepository';
