f202aee6bc97a09b044ce1aa228f7505d7ff3b68
netervati <netervati@gmail.com>
2023-02-26 15:10:35 +0800

    Add serialize method

--- server/errors/baseError.ts
+++ server/errors/baseError.ts
@@ -1,4 +1,11 @@
 export default class BaseError extends Error implements APIError {
   statusCode = 200;
   statusMessage = '';
+
+  serialize() {
+    return {
+      statusCode: this.statusCode,
+      statusMessage: this.statusMessage,
+    };
+  }
 }
