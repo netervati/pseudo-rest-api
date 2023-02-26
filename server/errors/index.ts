d862b3e3028089429b9a85596971da21ffe324ff
netervati <netervati@gmail.com>
2023-02-26 15:10:13 +0800

    Add validation error class

--- server/errors/index.ts
+++ server/errors/index.ts
@@ -2,3 +2,4 @@ export { default as BaseError } from './baseError';
 export { default as FailedDatabaseQueryError } from './failedDatabaseQueryError';
 export { default as InvalidParameterError } from './invalidParameterError';
 export { default as NotAuthenticatedError } from './notAuthenticatedError';
+export { default as ValidationError } from './validationError';
