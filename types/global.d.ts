14fda1649bbe38b65817347011ac5efef1f37fd2
netervati <netervati@gmail.com>
2023-02-26 15:16:54 +0800

    Update error types

--- types/global.d.ts
+++ types/global.d.ts
@@ -3,13 +3,18 @@ import { PostgrestSingleResponse } from '@supabase/postgrest-js';
 import { SupabaseClient } from '@supabase/supabase-js';
 
 declare global {
-  interface APIError extends Error {
+  interface SerializedError {
+    data?: { [key: string]: string }[];
     statusCode: number;
     statusMessage: string;
-  }
+  };
+
+  interface APIError extends Error, SerializedError {
+    serialize: () => SerializedError;
+  };
 
   type Result<T, E> = Ok<T, E> | Err<T, E>;
-  type ValidationResult = Result<void, APIError>;
+  type ValidationResult = undefined | SerializedError;
 
   type APIBody<T> = {
     attributes: T;
