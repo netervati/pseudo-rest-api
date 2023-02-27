658be3e566f058b70549127bc8836a8d6512324b
netervati <netervati@gmail.com>
2023-02-28 00:20:48 +0800

    Remove error classes

--- server/errors/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as BaseError } from './baseError';
-export { default as FailedDatabaseQueryError } from './failedDatabaseQueryError';
-export { default as InvalidParameterError } from './invalidParameterError';
-export { default as NotAuthenticatedError } from './notAuthenticatedError';
-export { default as ValidationError } from './validationError';
