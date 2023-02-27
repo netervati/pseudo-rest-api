749f0a531c25a8b5f0c146f3a941ea67aa5b82f2
netervati <netervati@gmail.com>
2023-02-27 22:29:33 +0800

    Format query result

--- server/repositories/baseRepository.ts
+++ server/repositories/baseRepository.ts
@@ -1,6 +1,16 @@
 import { H3Event } from 'h3';
+import {
+  PostgrestResponseFailure,
+  PostgrestResponseSuccess,
+} from '@supabase/postgrest-js';
+import { NuxtError } from 'nuxt/dist/app/composables';
 import { Database, Json } from '~~/types/supabase';
 import { serverSupabaseClient } from '#supabase/server';
+
+type QueryResponse<T extends Record<string, Json>> = {
+  data: T['Row'][] | null;
+  error: NuxtError | null;
+};
 
 export default class BaseRepository<T extends Record<string, Json>> {
   client;
@@ -15,17 +25,42 @@ export default class BaseRepository<T extends Record<string, Json>> {
     return '';
   }
 
-  async get(conditions = {}, select = '*'): RepositoryQueryResponse {
+  #format(
+    query: PostgrestResponseSuccess<T['Row'][]> | PostgrestResponseFailure
+  ): QueryResponse<T> {
+    const error =
+      query.error === null
+        ? null
+        : createError({
+            statusCode: HTTP_STATUS_BAD_REQUEST,
+            statusMessage: query.error.message,
+          });
+
+    return {
+      data: query.data,
+      error,
+    };
+  }
+
+  async get(conditions = {}, select = '*'): Promise<QueryResponse<T>> {
     const query = this.client.from(this.table).select(select);
 
     for (const [key, value] of Object.entries(conditions)) {
       query.eq(key, value);
     }
 
-    return await query.eq('user_id', this.userId);
+    // TODO: Currently, query results to GenericStringError[] when value passed
+    // on `.select()` is not a string literal. Remove the ignore comment once
+    // supabase supports string.
+    return this.#format(
+      // @ts-ignore
+      await query.eq('user_id', this.userId)
+    );
   }
 
-  async insert(data: T['Insert']): RepositoryQueryResponse {
-    return await this.client.from(this.table).insert(data).select();
+  async insert(data: T['Insert']): Promise<QueryResponse<T>> {
+    return this.#format(
+      await this.client.from(this.table).insert(data).select('*')
+    );
   }
 }
