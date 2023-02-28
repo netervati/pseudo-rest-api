9fc7febc80909af47492ba603151ec58ce04c685
netervati <netervati@gmail.com>
2023-02-28 20:50:09 +0800

    Add ResourceModel and ProjectKeyWithProject

--- types/models.ts
+++ types/models.ts
@@ -3,3 +3,9 @@ import { Database } from './supabase';
 export type Api = Database['public']['Tables']['apis']['Row'];
 export type ProjectKey = Database['public']['Tables']['project_keys']['Row'];
 export type Project = Database['public']['Tables']['projects']['Row'];
+export type ResourceModel =
+  Database['public']['Tables']['resource_models']['Row'];
+
+export type ProjectKeyWithProject = ProjectKey & {
+  projects: Project;
+};
