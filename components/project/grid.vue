205e4cbe9e38f89db274d8f4563860daf6f0cf27
netervati <netervati@gmail.com>
2023-03-02 00:03:36 +0800

    Utilize store

--- components/project/grid.vue
+++ components/project/grid.vue
@@ -1,32 +1,19 @@
 <script lang="ts" setup>
-  import { ProjectWithProjectKey } from '~~/types/models';
+  import useProjectStore from '~~/stores/useProjectStore';
 
   const props = defineProps<{
     secretKey: string;
   }>();
 
   const { secretKey } = toRefs(props);
-  const toast = useToast();
+  const projects = useProjectStore();
+  projects.fetch();
 
-  const { data, error, refresh } = await useFetch<ProjectWithProjectKey[]>(
-    '/projects',
-    {
-      method: 'get',
-    }
-  );
-
-  watch(error, (fetchError) => {
-    if (fetchError) {
-      toast.show(fetchError.statusMessage ?? 'Failed to retrieve projects', {
-        color: 'error',
-      });
+  watchEffect(async () => {
+    if (secretKey.value) {
+      await projects.fetch();
     }
   });
-
-  const projects = computed<ProjectWithProjectKey[]>(() => data.value || []);
-
-  onMounted(async () => await nextTick(refresh));
-  watch(secretKey, refresh);
 
   const handleOpen = (urlPath: string) => {
     navigateTo(`/project/${urlPath}/apis`);
@@ -36,7 +23,7 @@
 <template>
   <section class="gap-4 grid grid-cols-1 md:grid-cols-2 mt-4">
     <article
-      v-for="project in projects"
+      v-for="project in projects.list"
       :key="project.id"
       class="card border border-gray-300 hover:bg-gray-300"
       @click="handleOpen(project.project_keys[0].api_key)"
