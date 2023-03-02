83bb2fe20cbeaa22e95ac1930715c611c3d0eee9
netervati <netervati@gmail.com>
2023-03-03 02:53:31 +0800

    Add type to constant

--- server/utils/constants.ts
+++ server/utils/constants.ts
@@ -3,7 +3,7 @@ export const HTTP_STATUS_BAD_REQUEST = 400;
 export const HTTP_STATUS_UNAUTHORIZED = 401;
 
 // RESOURCE MODEL DATA TYPE
-export const RESOURCE_DATA_TYPES = {
+export const RESOURCE_DATA_TYPES: { [key: string]: string } = {
   data_type_boolean: 'Boolean',
   data_type_number: 'Number',
   data_type_string: 'String',
