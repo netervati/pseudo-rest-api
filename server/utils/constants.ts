2cefd88ffd47c6519122969a378772477c3eab30
netervati <netervati@gmail.com>
2023-03-03 00:36:08 +0800

    Add retrieve data types endpoint

--- server/utils/constants.ts
+++ server/utils/constants.ts
@@ -1,2 +1,11 @@
+// HTTP STATUS CODES
 export const HTTP_STATUS_BAD_REQUEST = 400;
 export const HTTP_STATUS_UNAUTHORIZED = 401;
+
+// RESOURCE MODEL DATA TYPE
+export const RESOURCE_DATA_TYPES = {
+  data_type_boolean: 'Boolean',
+  data_type_number: 'Number',
+  data_type_string: 'String',
+  data_type_uuid: 'UUID',
+};
