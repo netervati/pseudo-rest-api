100ca22ec0295cba76fa19ab1bd51c3880f1e5c9
netervati <netervati@gmail.com>
2023-03-05 05:19:00 +0800

    Support create project method

--- stores/useProjectStore.ts
+++ stores/useProjectStore.ts
@@ -2,14 +2,43 @@ import { Ref } from 'vue';
 import { defineStore } from 'pinia';
 import { ProjectWithProjectKey } from '~~/types/models';
 
+type BodyParams = {
+  description: string;
+  name: string;
+};
+
+type Options = {
+  onSuccess?: (key: string) => void;
+};
+
 type ProjectStore = {
   list: Ref<ProjectWithProjectKey[]>;
+  create: (body: BodyParams, options: Options) => Promise<void>;
   fetch: () => Promise<void>;
 };
 
 export default defineStore('projects', (): ProjectStore => {
   const list = ref<ProjectWithProjectKey[]>([]);
   const toast = useToast();
+
+  /**
+   * A function for creating project.
+   */
+  const create = async (body: BodyParams, options: Options): Promise<void> => {
+    await $fetch('/projects', {
+      method: 'POST',
+      body,
+      onResponse({ response }) {
+        toast.success('Created a project!');
+
+        if (typeof options.onSuccess === 'function') {
+          options.onSuccess(response._data.secret_key);
+        }
+      },
+    }).catch((error) => {
+      toast.error(error.statusMessage);
+    });
+  };
 
   /**
    * A function for fetching projects from the server.
@@ -27,6 +56,7 @@ export default defineStore('projects', (): ProjectStore => {
 
   return {
     list,
+    create,
     fetch,
   };
 });
