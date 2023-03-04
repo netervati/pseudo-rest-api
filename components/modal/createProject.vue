c94327decf9a753fbe950ba193dbf5b86382b18e
netervati <netervati@gmail.com>
2023-03-05 05:19:55 +0800

    Utilize useModalForm

--- components/modal/createProject.vue
+++ components/modal/createProject.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-  import { ProjectKey } from '~~/types/models';
+  // import { ProjectKey } from '~~/types/models';
+  import useProjectStore from '~~/stores/useProjectStore';
 
   const emit = defineEmits<{
     (e: 'close'): void;
@@ -10,6 +11,48 @@
     id: string;
   }>();
 
+  const project = useProjectStore();
+
+  type Form = {
+    description: string;
+    nameError: string;
+    name: string;
+    validations: {
+      name: string;
+    };
+  };
+
+  const form = useModalForm<Form>(
+    {
+      description: '',
+      nameError: '',
+      name: '',
+      validations: {
+        name: 'blank',
+      },
+    },
+    {
+      onClose: () => emit('close'),
+      onProceed: async (body: Omit<Form, 'validations'>) => {
+        await project.create(
+          {
+            description: body.description,
+            name: body.name,
+          },
+          {
+            onSuccess: (key: string) => {
+              emit('success', key);
+            },
+          }
+        );
+      },
+    }
+  );
+
+  const handleChange = () => {
+    form.fields.nameError = '';
+  };
+  /**
   const form = reactive({
     description: '',
     loading: false,
@@ -85,32 +128,35 @@
       form.showConfirm = true;
     }
   };
+  */
 </script>
 
 <template>
-  <ModalBase :id="id" @close="handleClose">
+  <ModalBase :id="id" @close="form.close">
     <h3 class="text-lg font-bold">Create a new Project</h3>
     <section class="form-control mt-2">
       <Input
-        v-model="form.name"
-        :error="form.nameError"
+        v-model="form.fields.name"
+        :error="form.fields.nameError !== ''"
         placeholder="Enter project name"
         @change="handleChange"
       />
-      <p v-if="form.nameError" class="text-red-600">Name cannot be blank.</p>
+      <p v-if="form.fields.nameError !== ''" class="text-red-600">
+        {{ form.fields.nameError }}
+      </p>
     </section>
     <section class="form-control mt-2">
       <textarea
-        v-model="form.description"
+        v-model="form.fields.description"
         class="textarea textarea-bordered"
         placeholder="Enter project description"
       />
     </section>
     <ModalFooter
-      :deps="{ showConfirm: form.showConfirm, loading: form.loading }"
-      @cancel="handleCancel"
-      @proceed="handleProceed"
-      @save="handleSave"
+      :deps="form.controls"
+      @cancel="form.cancel"
+      @proceed="form.proceed"
+      @save="form.save"
     />
   </ModalBase>
 </template>
