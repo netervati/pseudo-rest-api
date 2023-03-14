990d94c913a4d8ad8642b41dac95e76077ba9632
netervati <netervati@gmail.com>
2023-03-14 23:15:05 +0800

    Display resource data

--- components/resource/dataTable.vue
+++ components/resource/dataTable.vue
@@ -1,11 +1,46 @@
 <script lang="ts" setup>
   import ModalCreateResourceData from '../modal/createResourceData.vue';
-  import { useResourceModelStore } from '~~/stores';
+  import { useResourceDataStore, useResourceModelStore } from '~~/stores';
 
+  const resourceData = useResourceDataStore();
   const resourceModel = useResourceModelStore();
+  const structure = computed(() => {
+    // @ts-ignore
+    const list = resourceModel.list.filter(
+      (target) => target.id === resourceModel.target
+    );
+
+    if (list.length === 1) {
+      return list[0].structure;
+    }
+  });
+
+  const projectApiKey = useProjectApiKey() || '';
 
   const modal = useModal(ModalCreateResourceData, {
     id: 'create-resource-data',
+  });
+
+  onMounted(async () => {
+    if (resourceModel.target !== '' && resourceData.list.length === 0) {
+      await resourceData.fetch({
+        projectApiKey,
+        resourceModelId: resourceModel.target,
+      });
+    }
+  });
+
+  onUnmounted(() => {
+    resourceData.clear();
+  });
+
+  watchEffect(() => {
+    if (resourceModel.target !== '') {
+      resourceData.fetch({
+        projectApiKey,
+        resourceModelId: resourceModel.target,
+      });
+    }
   });
 </script>
 
@@ -24,11 +59,21 @@
               New Data
             </Button>
           </th>
+          <th v-for="header in structure" :key="header!.id">
+            {{ header!.name }}
+          </th>
         </tr>
       </thead>
       <tbody>
-        <tr>
+        <tr v-for="record in resourceData.list" :key="record.id">
           <td />
+          <td
+            v-for="// @ts-ignore
+              [key, value] in Object.entries(record.data)"
+            :key="key"
+          >
+            {{ value }}
+          </td>
         </tr>
       </tbody>
     </table>
