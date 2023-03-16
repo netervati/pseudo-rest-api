8c92fe08134e96825fa184edeb008e340bddfab9
netervati <netervati@gmail.com>
2023-03-16 21:12:46 +0800

    Override excessively deep nested types

--- types/models.ts
+++ types/models.ts
@@ -5,9 +5,15 @@ type Tables = Database['public']['Tables'];
 export type Api = Tables['apis']['Row'];
 export type ProjectKey = Tables['project_keys']['Row'];
 export type Project = Tables['projects']['Row'];
-export type ResourceData = Tables['resource_data']['Row'];
 
-export type ResourceModel = Tables['resource_models']['Row'] & {
+export type ResourceData = Omit<Tables['resource_data']['Row'], 'data'> & {
+  data: { [key: string]: string | number | boolean };
+};
+
+export type ResourceModel = Omit<
+  Tables['resource_models']['Row'],
+  'structure'
+> & {
   structure: {
     default: string | number | boolean;
     id: string;
