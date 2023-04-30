cc996ea9947f636a793a86168cc5deffb7a4d926
netervati <netervati@gmail.com>
2023-05-01 03:39:12 +0800

    Rework project modal

--- components/modal/createProject.vue
+++ components/modal/createProject.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-  import { UnwrapNestedRefs } from 'nuxt/dist/app/compat/capi';
   import useProjectStore from '~~/stores/useProjectStore';
+  import { isRequired } from '~~/utils/formValidations';
 
   const emit = defineEmits<{
     (e: 'close'): void;
@@ -11,75 +11,58 @@
     id: string;
   }>();
 
+  const form = useForm();
   const project = useProjectStore();
+  const isDisabled = computed(() => form.isSubmitting.value === true);
 
-  type Form = {
-    description: string;
-    nameError: string;
-    name: string;
-    validations: {
-      name: string;
-    };
+  const handleClose = () => {
+    form.resetForm();
+    emit('close');
   };
 
-  const form = useModalForm<Form>(
-    {
-      description: '',
-      nameError: '',
-      name: '',
-      validations: {
-        name: 'blank',
+  const onSubmit = form.handleSubmit(async (values) => {
+    await project.create(values, {
+      onSuccess: (key: string) => {
+        emit('success', key);
+        handleClose();
       },
-    },
-    {
-      onClose: () => emit('close'),
-      onProceed: async (body: UnwrapNestedRefs<Omit<Form, 'validations'>>) => {
-        await project.create(
-          {
-            description: body.description,
-            name: body.name,
-          },
-          {
-            onSuccess: (key: string) => {
-              emit('success', key);
-            },
-          }
-        );
-      },
-    }
-  );
-
-  const handleChange = () => {
-    form.fields.nameError = '';
-  };
+    });
+  });
 </script>
 
 <template>
-  <ModalBase :id="id" @close="form.close">
-    <h3 class="text-lg font-bold">Create a new Project</h3>
-    <section class="form-control mt-2">
-      <Input
-        v-model="form.fields.name"
-        :error="form.fields.nameError !== ''"
-        placeholder="Enter project name"
-        @change="handleChange"
-      />
-      <p v-if="form.fields.nameError !== ''" class="text-red-600">
-        {{ form.fields.nameError }}
-      </p>
-    </section>
-    <section class="form-control mt-2">
-      <textarea
-        v-model="form.fields.description"
-        class="textarea textarea-bordered"
-        placeholder="Enter project description"
-      />
-    </section>
-    <ModalFooter
-      :deps="form.controls"
-      @cancel="form.cancel"
-      @proceed="form.proceed"
-      @save="form.save"
-    />
+  <ModalBase :id="id" @close="handleClose">
+    <form @submit="onSubmit">
+      <h3 class="text-lg font-bold">Create a new Project</h3>
+      <section class="form-control mt-2">
+        <FormInput
+          :disabled="isDisabled"
+          name="name"
+          placeholder="Enter name"
+          :rule="isRequired('Name is required.')"
+        />
+      </section>
+      <section class="form-control mt-2">
+        <FormTextArea
+          :disabled="isDisabled"
+          name="description"
+          placeholder="Enter description"
+        />
+      </section>
+      <section class="mt-10">
+        <Button :disabled="isDisabled" size="sm" @click="handleClose">
+          Cancel
+        </Button>
+        <Button
+          :disabled="isDisabled"
+          class="float-right"
+          color="success"
+          size="sm"
+          type="submit"
+        >
+          Proceed
+        </Button>
+      </section>
+    </form>
   </ModalBase>
 </template>
