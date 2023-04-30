89c05101454960eded178d1bedd54fbc1935f9c5
netervati <netervati@gmail.com>
2023-05-01 04:02:33 +0800

    Create generic text area component

---
+++ components/textArea.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+  const emit = defineEmits<{
+    (e: 'change'): void;
+    (e: 'update:modelValue', value: string): void;
+  }>();
+
+  interface Props {
+    class?: string;
+    disabled?: boolean;
+    error?: boolean;
+    modelValue: string;
+    placeholder?: string;
+  }
+
+  const props = withDefaults(defineProps<Props>(), {
+    class: '',
+    disabled: false,
+    error: false,
+    placeholder: '',
+  });
+
+  const textAreaValue = computed({
+    get() {
+      return props.modelValue;
+    },
+    set(value: string) {
+      emit('change');
+      emit('update:modelValue', value);
+    },
+  });
+</script>
+
+<template>
+  <textarea
+    v-model="textAreaValue"
+    class="textarea textarea-bordered"
+    :disabled="disabled"
+    :placeholder="props.placeholder"
+  />
+</template>
