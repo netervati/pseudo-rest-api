423f810af926df9b2db84e2202772f29634c18cf
netervati <netervati@gmail.com>
2023-05-01 03:38:33 +0800

    Create form fields

---
+++ components/form/input.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+  const props = defineProps<{
+    disabled: boolean;
+    name: string;
+    placeholder: string;
+    rule?: (value: string) => boolean | string;
+  }>();
+
+  const validate = props.rule || (() => true);
+  const { value, errorMessage } = useField(props.name, validate);
+</script>
+
+<template>
+  <Input
+    v-model="value"
+    :disabled="props.disabled"
+    :error="errorMessage !== undefined"
+    :placeholder="props.placeholder"
+  />
+  <p class="text-red-600">
+    {{ errorMessage }}
+  </p>
+</template>
---
+++ components/form/textArea.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+  const props = defineProps<{
+    disabled: boolean;
+    name: string;
+    placeholder: string;
+  }>();
+
+  const { value } = useField(props.name);
+</script>
+
+<template>
+  <textarea
+    v-model="value"
+    :disabled="props.disabled"
+    class="textarea textarea-bordered"
+    placeholder="Enter project description"
+  />
+</template>
