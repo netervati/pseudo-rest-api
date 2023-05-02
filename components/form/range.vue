5a95cf5a2449e4b758f87be6690566876c2fb820
netervati <netervati@gmail.com>
2023-05-02 12:58:03 +0800

    Create form range component

---
+++ components/form/range.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+  import { FormValidator, runValidations } from '~~/utils/formValidations';
+
+  const emit = defineEmits<{
+    (e: 'change', value: string): void;
+  }>();
+
+  const props = defineProps<{
+    disabled: boolean;
+    max: number;
+    min: number;
+    name: string;
+    rules?: { [key: string]: FormValidator };
+    value?: string;
+  }>();
+
+  const validate = !props.rules ? () => true : runValidations(props.rules);
+  const { value, errorMessage } = useField(() => props.name, validate);
+
+  watchEffect(() => {
+    if (props.value) {
+      value.value = props.value;
+    }
+  });
+</script>
+
+<template>
+  <input
+    v-model="value"
+    :disabled="props.disabled"
+    :max="props.max"
+    :min="props.min"
+    class="range range-xs"
+    type="range"
+    @change="emit('change', value)"
+  />
+  <p class="text-red-600">
+    {{ errorMessage }}
+  </p>
+  <section class="grid grid-cols-2">
+    <div>{{ value }}</div>
+    <div class="text-end">{{ props.max }}</div>
+  </section>
+</template>
