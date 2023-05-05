00687a13d2bd1a9317a9e9437c5c00496c083251
netervati <netervati@gmail.com>
2023-05-06 01:23:17 +0800

    Refresh table when new api is created

--- pages/project/[url-path]/apis.vue
+++ pages/project/[url-path]/apis.vue
@@ -5,15 +5,20 @@
     middleware: 'auth',
   });
 
+  const refresh = ref(Date.now());
+
   const modal = useModal(ModalCreateApi, {
     id: 'create-api',
+    onSuccess: () => {
+      refresh.value = Date.now();
+    },
   });
 </script>
 
 <template>
   <div class="p-6">
     <Button color="success" size="sm" @click="modal.open">New API</Button>
-    <ApiTable />
+    <ApiTable :key="refresh" />
     <ClientOnly>
       <modal.component />
     </ClientOnly>
