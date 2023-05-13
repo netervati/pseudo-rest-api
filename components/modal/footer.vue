cd69ee95deb722c202ace9fa9c6407c5c7957b3a
netervati <netervati@gmail.com>
2023-05-13 15:45:13 +0800

    Create template footer

---
+++ components/modal/footer.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  defineProps<{
+    disabled: boolean;
+  }>();
+
+  const emit = defineEmits<{
+    (e: 'close'): void;
+  }>();
+</script>
+
+<template>
+  <section class="mt-10">
+    <Button :disabled="disabled" size="sm" @click="emit('close')">
+      Cancel
+    </Button>
+    <Button
+      :loading="disabled"
+      class="float-right"
+      color="success"
+      size="sm"
+      type="submit"
+    >
+      Proceed
+    </Button>
+  </section>
+</template>
