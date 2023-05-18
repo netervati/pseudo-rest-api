8a41fab973e3fe5f5e843ec8a05e8899f1f25292
netervati <netervati@gmail.com>
2023-05-18 21:17:51 +0800

    Create project secret box reusable component

---
+++ components/project/secretKeyBox.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+  defineProps<{
+    secretKey: string;
+  }>();
+</script>
+
+<template>
+  <article
+    v-if="secretKey.trim() !== ''"
+    class="bg-emerald-400 p-8 rounded-lg mt-4"
+  >
+    <p>
+      Below is your project's secret key. Make sure to copy this as you will
+      only see this once!
+    </p>
+    <div class="mt-2">
+      <input class="bg-emerald-300 input w-full" :value="secretKey" />
+    </div>
+  </article>
+</template>
