b722f52d8356bdecf2de9fc426d2f0bcff60f046
netervati <netervati@gmail.com>
2023-05-18 21:18:18 +0800

    Mount components and composables

--- pages/project/[url-path]/settings.vue
+++ pages/project/[url-path]/settings.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
-  import useProjectStore from '~~/stores/useProjectStore';
+  import { useProjectStore, useProjectKeyStore } from '~~/stores';
   import { isRequired } from '~~/utils/formValidations';
   import { ProjectWithProjectKey } from '~~/types/models';
+  import ModalConfirm from '~~/components/modal/confirm.vue';
 
   const projectApiKey = useProjectApiKey() || '';
   const project = useProjectStore();
+  const projectKey = useProjectKeyStore();
   const currentProject = ref<ProjectWithProjectKey>();
 
   const form = useForm();
@@ -40,11 +42,33 @@
       }
     );
   });
+
+  const secretKey = ref('');
+
+  const generateSecretKeyModal = useModal(ModalConfirm, {
+    id: 'confirm-generate-secret-key',
+    onConfirm: async (callback: () => void) => {
+      await projectKey.regenerate(
+        {
+          id: currentProject.value?.id,
+          projectApiKey,
+        },
+        {
+          onSuccess: (key: string) => {
+            secretKey.value = key;
+          },
+        }
+      );
+
+      callback();
+    },
+  });
 </script>
 
 <template>
-  <div class="p-6">
-    <div class="card border border-gray-300">
+  <div class="pl-6 pr-6">
+    <ProjectSecretKeyBox :secret-key="secretKey" />
+    <div class="card border border-gray-300 mt-6">
       <div class="card-body">
         <form @submit="onSubmit">
           <h3 class="font-bold">Edit Project</h3>
@@ -75,5 +99,22 @@
         </form>
       </div>
     </div>
+    <div class="card border border-gray-300 mt-6">
+      <div class="card-body">
+        <Button
+          class="w-100"
+          color="error"
+          @click="generateSecretKeyModal.open()"
+        >
+          Generate new Secret Key
+        </Button>
+      </div>
+    </div>
+    <ClientOnly>
+      <component
+        :is="generateSecretKeyModal.component"
+        content="Are you sure you want to generate a new secret key?"
+      />
+    </ClientOnly>
   </div>
 </template>
