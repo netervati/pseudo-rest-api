298ef0282e210ff9372e181c19ce0499a2383913
netervati <netervati@gmail.com>
2023-05-27 19:13:54 +0800

    Create base request composable for stores

---
+++ stores/useBaseRequest.ts
@@ -0,0 +1,29 @@
+import { NitroFetchOptions } from 'nitropack';
+
+export default function () {
+  const isLoading = ref(false);
+  const toast = useToast();
+
+  /**
+   * NOTE: This generic type doesn't validate if HTTP Method is
+   * allowed for url path correctly. Consider revisting this again.
+   */
+  const request = async <T extends string = `/_${string}`>(
+    path: string,
+    config: NitroFetchOptions<T>
+  ) => {
+    isLoading.value = true;
+
+    await $fetch(path, config).catch((error) =>
+      toast.error(error.statusMessage)
+    );
+
+    isLoading.value = false;
+  };
+
+  return {
+    isLoading,
+    request,
+    toast,
+  };
+}
