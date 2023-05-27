7107149284fb416ff23ce00f35329692e96a800f
netervati <netervati@gmail.com>
2023-05-27 22:27:59 +0800

    Add more faker options

--- server/utils/generateResourceData.ts
+++ server/utils/generateResourceData.ts
@@ -21,18 +21,36 @@ function setValue(field: Structure): string | number | boolean {
   switch (field.type) {
     case 'data_type_uuid':
       return uuidv4();
+    case 'faker_animal':
+      return faker.animal.type();
+    case 'faker_city':
+      return faker.location.city();
+    case 'faker_color':
+      return faker.color.human();
+    case 'faker_country':
+      return faker.location.country();
     case 'faker_date':
       return faker.date.anytime().toISOString().substring(0, 10);
     case 'faker_email_address':
       return faker.internet.email();
     case 'faker_full_name':
       return faker.person.fullName();
+    case 'faker_job':
+      return faker.person.jobType();
+    case 'faker_phone_number':
+      return faker.phone.number();
     case 'faker_price':
       return Number(faker.commerce.price());
     case 'faker_product_name':
       return faker.commerce.productName();
     case 'faker_product':
       return faker.commerce.product();
+    case 'faker_street':
+      return faker.location.streetAddress();
+    case 'faker_vehicle_manufacturer':
+      return faker.vehicle.manufacturer();
+    case 'faker_vehicle':
+      return faker.vehicle.type();
     default:
       return field.default;
   }
