068088cdb2650478dd1483304980e63f83bdb479
netervati <netervati@gmail.com>
2023-05-27 12:33:05 +0800

    Require user to be authenticated when deleting project

--- server/routes/projects/[id].delete.ts
+++ server/routes/projects/[id].delete.ts
@@ -1,5 +1,9 @@
 import ProjectServices from '~~/server/services/projectServices';
 
 export default defineEventHandler(async (event) => {
+  if (event.context.auth.error) {
+    throw event.context.auth.error;
+  }
+
   return await new ProjectServices(event).delete(event.context.params.id);
 });
