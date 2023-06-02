a51f1081d6d850dd21096056bb50676d811ac053
netervati <netervati@gmail.com>
2023-06-02 20:07:17 +0800

    Throw error within middleware

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -2,14 +2,13 @@ import { serverSupabaseUser } from '#supabase/server';
 
 export default defineEventHandler(async (event) => {
   const user = await serverSupabaseUser(event);
-  let error = null;
 
   if (user === null) {
-    error = createError({
+    throw createError({
       statusCode: HTTP_STATUS_UNAUTHORIZED,
       statusMessage: 'User is not authenticated.',
     });
   }
 
-  event.context.auth = { error, user };
+  event.context.auth = { user };
 });
