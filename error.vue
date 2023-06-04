cfe586501d1ef72f2153e599164eb212b2121a7d
netervati <netervati@gmail.com>
2023-06-04 10:12:36 +0800

    Display maintenance page when site is under maintenance

---
+++ error.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import { H3Error } from 'h3';
+  import { ExclamationTriangleIcon } from '@heroicons/vue/24/outline';
+
+  defineProps<{
+    error: H3Error;
+  }>();
+</script>
+
+<template>
+  <div class="flex w-screen h-screen">
+    <section class="m-auto text-center text-xl">
+      <p>
+        <ExclamationTriangleIcon class="m-auto w-8 h-8" />
+        {{ error.statusMessage }}
+      </p>
+    </section>
+  </div>
+</template>
