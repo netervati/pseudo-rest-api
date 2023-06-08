18cc859b0b2a63ff208b1d11fed63940f1ed0e7d
netervati <netervati@gmail.com>
2023-06-08 18:52:00 +0800

    Create bulk delete resource data endpoint

---
+++ server/routes/resource-models/[id]/resource-data.delete.ts
@@ -0,0 +1,22 @@
+import ResourceDataServices from '~~/server/services/resourceDataServices';
+import extractProjectKey from '~~/server/lib/extractProjectKey';
+
+type QueryParams = {
+  ids: string;
+  projectApiKey: string;
+};
+
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event) as QueryParams;
+
+  await extractProjectKey(event, query.projectApiKey);
+
+  const ids = query.ids.split(',');
+
+  const deleted = await new ResourceDataServices(event).batchDelete({
+    ids,
+    resourceModelId: event.context.params.path.id,
+  });
+
+  return deleted;
+});
