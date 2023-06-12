27b11c207292cef11003202bed750a8a1d461bbc
netervati <netervati@gmail.com>
2023-06-12 23:30:48 +0800

    Create home and introduction pages for docs

---
+++ pages/docs/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'docs',
+  });
+</script>
+
+<template>
+  <section class="p-6">
+    <DocTitle>PseudoRESTAPI - Official Documentation</DocTitle>
+    <DocParagraph>
+      This is the official guide to <b>PseudoRESTAPI.</b>
+    </DocParagraph>
+    <DocParagraph>
+      You can start by reading the <b>Getting Started</b> to understand the
+      basic concepts and usages so you can immediately begin using the
+      application.
+    </DocParagraph>
+    <DocParagraph>
+      Other documents detail advance usages, and other information that you
+      might want to check later to better understand the tool.
+    </DocParagraph>
+  </section>
+</template>
---
+++ pages/docs/introduction.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'docs',
+  });
+</script>
+
+<template>
+  <section class="p-6">
+    <DocTitle>Introduction</DocTitle>
+    <DocParagraph>
+      <b>PseudoRESTAPI</b> is a passion project that I have built and continue
+      to maintain in my free time.
+    </DocParagraph>
+    <DocParagraph>
+      I had three goals in mind when I started with this project:
+    </DocParagraph>
+    <ul class="list-disc list-inside">
+      <li class="pb-3 pt-3">
+        To create a valuable tool for developers and QA engineers. One common
+        pain point that developers experience when creating client prototypes is
+        the need for server fetching. While developers with backend engineering
+        knowledge can create their own server applications, the effort may not
+        always be justifiable, especially when time constraints are present. To
+        address this, I developed <b>PseudoRESTAPI</b> as a platform that offers
+        a faster way to develop and deploy mock servers. Additionally,
+        professionals like QA engineers can benefit from this tool for
+        practicing manual and automated API testing.
+      </li>
+      <li class="pb-3 pt-3">
+        To learn Nuxt3 with Supabase. Vue3 is one of my favorite front-end
+        frameworks, and when Nuxt3 was officially released in 2022, I was
+        thrilled to learn and utilize this technology in a project.
+        Additionally, I have been tinkering with Supabase for over a year now,
+        making it a natural fit to combine both technologies for this project.
+      </li>
+      <li class="pb-3 pt-3">
+        To deploy a "production-ready" open-source project. While I have
+        previously worked on personal open-source projects, only Space
+        Distortion was released as production-ready. Since 2022, I have been
+        eager to start another project, and <b>PseudoRESTAPI</b> presented the
+        perfect opportunity for me to do so.
+      </li>
+    </ul>
+    <DocParagraph>
+      That is the brief history of this project. To get started with
+      <b>PseudoRESTAPI</b>, please continue here.
+    </DocParagraph>
+  </section>
+</template>
