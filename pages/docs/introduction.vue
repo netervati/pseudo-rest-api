f74ee21a92818764f88bc0a2c8ad6da5a7a50292
netervati <netervati@gmail.com>
2023-06-13 17:47:56 +0800

    Use list components

--- pages/docs/introduction.vue
+++ pages/docs/introduction.vue
@@ -14,8 +14,8 @@
     <DocParagraph>
       I had three goals in mind when I started with this project:
     </DocParagraph>
-    <ul class="list-disc list-inside">
-      <li class="pb-3 pt-3">
+    <DocList>
+      <DocListItem>
         To create a valuable tool for developers and QA engineers. One common
         pain point that developers experience when creating client prototypes is
         the need for server fetching. While developers with backend engineering
@@ -25,22 +25,22 @@
         a faster way to develop and deploy mock servers. Additionally,
         professionals like QA engineers can benefit from this tool for
         practicing manual and automated API testing.
-      </li>
-      <li class="pb-3 pt-3">
+      </DocListItem>
+      <DocListItem>
         To learn Nuxt3 with Supabase. Vue3 is one of my favorite front-end
         frameworks, and when Nuxt3 was officially released in 2022, I was
         thrilled to learn and utilize this technology in a project.
         Additionally, I have been tinkering with Supabase for over a year now,
         making it a natural fit to combine both technologies for this project.
-      </li>
-      <li class="pb-3 pt-3">
+      </DocListItem>
+      <DocListItem>
         To deploy a "production-ready" open-source project. While I have
         previously worked on personal open-source projects, only Space
         Distortion was released as production-ready. Since 2022, I have been
         eager to start another project, and <b>PseudoRESTAPI</b> presented the
         perfect opportunity for me to do so.
-      </li>
-    </ul>
+      </DocListItem>
+    </DocList>
     <DocParagraph>
       That is the brief history of this project. To get started with
       <b>PseudoRESTAPI</b>, please continue here.
