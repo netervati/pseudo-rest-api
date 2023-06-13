550ba99722b0b3c661a6285134db5069bc8e18aa
netervati <netervati@gmail.com>
2023-06-13 17:47:36 +0800

    Create reusable doc list components

---
+++ components/doc/list.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+  interface Props {
+    class?: string;
+  }
+
+  const props = withDefaults(defineProps<Props>(), {
+    class: '',
+  });
+
+  const mergedClass = computed(() => {
+    return {
+      [props.class]: props.class !== '',
+    };
+  });
+</script>
+
+<template>
+  <ul :class="mergedClass" class="list-disc list-inside">
+    <slot />
+  </ul>
+</template>
