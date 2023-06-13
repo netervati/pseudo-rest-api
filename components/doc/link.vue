ad688713bd89ffe66b6bb6da6d566c4eb57a8635
netervati <netervati@gmail.com>
2023-06-13 22:02:23 +0800

    Create doc link component

---
+++ components/doc/link.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+  defineProps<{
+    to: string;
+  }>();
+</script>
+
+<template>
+  <NuxtLink
+    class="decoration-2 font-bold text-success underline underline-offset-2"
+    :to="to"
+  >
+    <slot />
+  </NuxtLink>
+</template>
