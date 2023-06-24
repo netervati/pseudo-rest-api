6100d7e0ddc8b39dc037b8be1e1ddeefb7782b1c
netervati <netervati@gmail.com>
2023-06-24 08:54:02 +0800

    Move sign in message in auth middlware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,12 @@
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware((to, from) => {
   const user = useSupabaseUser();
+  const toast = useToast();
 
   if (!user.value) {
     return navigateTo('/login');
+  }
+
+  if (from.path === '/login' && to.path === '/') {
+    toast.dark('Successfully signed in');
   }
 });
