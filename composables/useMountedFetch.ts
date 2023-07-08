8bd098c059e1f5d66a154214719e8e4732d242c1
netervati <netervati@gmail.com>
2023-07-08 17:30:50 +0800

    Rename type for stores

--- composables/useMountedFetch.ts
+++ composables/useMountedFetch.ts
@@ -1,9 +1,9 @@
-type CommonStore = {
+type DefaultStoreProps = {
   fetch: () => Promise<void>;
 };
 
 /** Composable meant to handle store fetches on component mount */
-export default function (stores: Array<CommonStore>) {
+export default function (stores: Array<DefaultStoreProps>) {
   onMounted(async () => {
     await Promise.all(stores.map((store) => store.fetch()));
   });
