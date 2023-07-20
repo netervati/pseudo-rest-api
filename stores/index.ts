343ff5ccf80a457663bace1639d9683e961a260e
netervati <netervati@gmail.com>
2023-07-20 20:14:19 +0800

    Remove old resource data store

--- stores/index.ts
+++ stores/index.ts
@@ -1,7 +1,7 @@
 export { default as useProjectKeyStore } from './useProjectKeyStore';
-export { default as useResourceDataStore } from './useResourceDataStore';
 
 export { default as useApi } from './useApi';
 export { default as useProject } from './useProject';
 export { default as useResourceDataType } from './useResourceDataType';
+export { default as useResourceData } from './useResourceData';
 export { default as useResourceModel } from './useResourceModel';
