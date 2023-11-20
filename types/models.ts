02d958b47ec01a12309494d4d2000cbe463b8d01
Netervati <netervati@gmail.com>
2023-11-20 17:49:16 +0800

    Remove missed file and types

--- types/models.ts
+++ types/models.ts
@@ -2,40 +2,6 @@ import { Database } from './supabase';
 
 type Tables = Database['public']['Tables'];
 
-export type Api = Tables['apis']['Row'];
-export type ProjectKey = Tables['project_keys']['Row'];
-export type Project = Tables['projects']['Row'];
-
-export type ResourceData = Omit<Tables['resource_data']['Row'], 'data'> & {
-  data: { [key: string]: string | number | boolean };
-};
-
-export type ResourceModel = Omit<
-  Tables['resource_models']['Row'],
-  'structure'
-> & {
-  structure: {
-    default: string | number | boolean;
-    id: string;
-    name: string;
-    type: string;
-  }[];
-};
-
-export type ApiWithResourceModel = Api & {
-  resource_models: ResourceModel;
-};
-
-export type ProjectKeyWithProject = ProjectKey & {
-  projects: Project;
-};
-
-export type ProjectWithProjectKey = Project & {
-  project_keys: ProjectKey[];
-};
-
-// ===================== REVAMP
-
 export type AppKey = Tables['app_keys']['Row'];
 export type App = Tables['apps']['Row'];
 
