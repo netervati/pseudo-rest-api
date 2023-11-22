22765ea07e691354b7d39a82843436f23cafd87e
Netervati <netervati@gmail.com>
2023-11-22 23:01:12 +0800

    Update docs UI

--- components/content/ProseLi.vue
+++ components/content/ProseLi.vue
@@ -1,3 +1,3 @@
 <template>
-  <li class="mb-6 mt-6"><slot /></li>
+  <li class="mb-6 mt-6 md:mb-8 md:mt-8 md:text-lg"><slot /></li>
 </template>
--- components/logoLink.vue
+++ components/logoLink.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
   type Props = {
+    class?: string;
+    isButton?: boolean;
     to?: string;
   };
 
   const props = withDefaults(defineProps<Props>(), {
+    class: '',
+    isButton: true,
     to: '/',
   });
+
+  const mergeClass = () => {
+    return {
+      'btn btn-ghost': props.isButton,
+      [props.class]: props.class !== '',
+    }
+  }
 </script>
 
 <template>
   <NuxtLink
     :to="props.to"
-    class="btn btn-ghost gap-x-2 normal-case text-xl"
+    :class="mergeClass()"
+    class="font-bold gap-x-2 normal-case text-xl"
   >
     <img width="36" src="/logo.png" /> PseudoRESTAPI
   </NuxtLink>
